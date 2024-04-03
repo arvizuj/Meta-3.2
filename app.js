06b6035b2f7c28bd19b535fd6b68c52c0fc539d0
arvizuj <arvizuj@uabc.edu.mx>
2024-04-03 11:47:50 -0700

    new file:   routes/activos.js 	new file:   routes/responsables.js 	new file:   routes/ubicaciones.js

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const app = express();
+const port = 4000;
+
+app.use(express.json());
+
+const activosRouter = require('./routes/activos');
+const responsablesRouter = require('./routes/responsables');
+const ubicacionesRouter = require('./routes/ubicaciones');
+
+app.use('/activos', activosRouter);
+app.use('/responsables', responsablesRouter);
+app.use('/ubicaciones', ubicacionesRouter);
+
+app.get('/', (req, res) => {
+    res.send('Meta 3.1 Implementar servicio de web REST')
+});
+
+app.listen(port, () => {
+    console.log('Servidor escuchando puerto: ', port)
+}).on('error', err => {
+    console.log('Error al iniciar servidor: ', err)
+});
